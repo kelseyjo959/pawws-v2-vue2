bf39a768803b8977449761209367724ad6349314
Kelsey Holt <kelseyjo959@gmail.fm>
2019-01-11 09:41:55 -0600

    added PetDetails component

---
+++ src/components/PetDetails.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="pet">
+    <h4>{{ pet.name }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PetDetails',
+  props: {
+    pet: Object
+  }
+}
+</script>
