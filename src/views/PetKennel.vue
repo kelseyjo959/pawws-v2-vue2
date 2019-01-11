c996e823fb12e331def1dcdac38d44f3c54451b0
Kelsey Holt <kelseyjo959@gmail.fm>
2019-01-11 09:40:26 -0600

    added PetKennel view

---
+++ src/views/PetKennel.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="pet-kennel">
+    <h1>This the pets page</h1>
+    <button v-on:click="getPets">Click for pets</button>
+    <PetDetails v-for="pet in pets" :key="pet.name" :pet="pet" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import PetDetails from '../components/PetDetails.vue'
+export default {
+  name: 'pet-kennel',
+  components: {
+    PetDetails
+  },
+  data () {
+    return {
+      pets: []
+    }
+  },
+  methods: {
+    getPets: function () {
+      console.log('I was clicked')
+      return axios.get('http://localhost:3000/getPets', {
+        headers: {
+          "screenSize": window.width,
+          "shelter": "",
+          "count": 0,
+        }
+      })
+        .then((response) => {
+          this.pets = response.data;
+        }) 
+        // (error) => {
+        //   // eslint-disable-next-line
+        //   console.log(error);
+        // })
+    }
+  }
+}
+</script>
