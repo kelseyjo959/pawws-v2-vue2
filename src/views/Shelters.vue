f934621fe819a2e02ff19e040be55b6228009910
Kelsey Holt <kelseyjo959@gmail.fm>
2019-01-28 10:26:44 -0600

    testing out imports to Shelters view

--- src/views/Shelters.vue
+++ src/views/Shelters.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="shelters">
     <p>This is the shelter page</p>
+    <StrayRescue />
+    <HelloWorld msg="Hello" />
   </div>
 </template>
 
 <script>
+import StrayRescue from "../components/StrayRescue.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+
 export default {
-  name: "shelters"
+  name: "shelters",
+  components: {
+    HelloWorld,
+    StrayRescue
+  }
 };
 </script>
