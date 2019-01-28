0ae7af3b6c83fad09d05869b6130875fd0afc206
Kelsey Holt <kelseyjo959@gmail.fm>
2019-01-28 11:06:44 -0600

    changed theme name to be more pertinent to view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,11 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <md-card>
+    <md-card md-theme="home">
       <md-card-header>
         <div class="md-title">Material Design with Vue.js 2 Demo</div>
         <div class="md-subhead">by CodingTheSmartWay.com</div>
-        <md-button md-theme="default-app" class="md-raised md-primary">Test Button</md-button>
+        <md-button class="md-raised md-primary">Test Button</md-button>
       </md-card-header>
     </md-card>
   </div>
@@ -27,14 +27,14 @@ export default {
 <style lang="scss" scoped>
 @import "~vue-material/dist/theme/engine";
 @include md-register-theme(
-  "default",
+  "home",
   (
     primary: md-get-palette-color(cyan, 800),
     accent: md-get-palette-color(brown, 500),
     theme: light
   )
 );
-@import "~vue-material/dist/components/MdCard/theme";
+@import "~vue-material/dist/theme/all";
 .md-card {
   width: 500px;
   display: inline-block;
